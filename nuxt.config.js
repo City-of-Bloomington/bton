1fa87646763652e173c60d2e272e657447da0a57
adabuch <adabutch1@gmail.com>
2020-08-19 07:34:18 -0400

    vuex user roles

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mode: 'universal',
+  mode: "universal",
   // server: {
   //   port: 443
   // },
@@ -8,82 +8,77 @@ module.exports = {
     apiHost: process.env.API_HOST,
     clientHost: process.env.CLIENT_HOST,
     cobWebsiteURL: process.env.CITY_WEBSITE_URL,
+    userRoles: process.env.USER_ROLES
   },
 
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   manifest: {
-    name: 'Bton - City of Bloomington, Indiana, online services.',
-    short_name: 'Bton',
-    theme_color: '#1e59ae',
-    background_color: '#1e5aae',
-    display: 'standalone',
-    description: 'Shorten City of Bloomington URLs using Bton.'
+    name: "Bton - City of Bloomington, Indiana, online services.",
+    short_name: "Bton",
+    theme_color: "#1e59ae",
+    background_color: "#1e5aae",
+    display: "standalone",
+    description: "Shorten City of Bloomington URLs using Bton."
   },
 
-  loading: { color: '#fff' },
+  loading: { color: "#fff" },
 
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
+    "@nuxtjs/axios",
+    "@nuxtjs/pwa",
     // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv',
-    '@nuxtjs/style-resources',
-    'cookie-universal-nuxt',
+    "@nuxtjs/dotenv",
+    "@nuxtjs/style-resources",
+    "cookie-universal-nuxt"
   ],
 
-  css: ['assets/scss/style.scss'],
+  css: ["assets/scss/style.scss"],
 
   styleResources: {
-    scss: [
-      '~assets/scss/system.utils.scss',
-    ]
+    scss: ["~assets/scss/system.utils.scss"]
   },
 
   plugins: [
-    { src: '~/plugins/design-system' },
-    { src: '~/plugins/universal-methods' },
+    { src: "~/plugins/design-system" },
+    { src: "~/plugins/universal-methods" },
     // { src: '~/plugins/click-outside' },
-    { src: '~/plugins/google-analytics.js', ssr: false },
+    { src: "~/plugins/google-analytics.js", ssr: false }
     // { src: '~/plugins/persistedState.js',   ssr: false },
     // { src: '~/plugins/vue-cropper',         ssr: false },
     // { src: '~/plugins/vuedraggable.js',     ssr: false }
   ],
 
   /*
-  ** Nuxt.js dev-modules
-  */
+   ** Nuxt.js dev-modules
+   */
   buildModules: [],
 
   /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {
-  },
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
 
-
-
-  serverMiddleware: [
-    { path: '/api', handler: '~/server/api/index.js' }
-  ],
+  serverMiddleware: [{ path: "/api", handler: "~/server/api/index.js" }],
   build: {
     // transpile: ['vue-cropperjs'],
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    },
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
--- store/index.js
+++ store/index.js
@@ -1,46 +1,50 @@
-import Vuex from 'vuex'
-import axios from 'axios'
+import Vuex from "vuex";
+import axios from "axios";
 
-import {
-  getField,
-  updateField
-} from 'vuex-map-fields'
+import { getField, updateField } from "vuex-map-fields";
 
 export const strict = false;
 
 export const defaultState = () => ({
-  systemRoles: JSON.parse(process.env.USER_ROLES),
+  systemRoles: JSON.parse(process.env.userRoles),
   authenticated: false,
   user: null,
   appData: null,
   appDataLogo: null,
   appDataScreenshots: [],
   consoleLog: {
-    info: ['background: rgb(31, 90, 174)',
-      'color: white',
-      'display: block',
-      'border-radius: 3px',
-      'padding: 2px 0'].join(';'),
+    info: [
+      "background: rgb(31, 90, 174)",
+      "color: white",
+      "display: block",
+      "border-radius: 3px",
+      "padding: 2px 0"
+    ].join(";"),
 
-    success: ['background: rgb(76, 174, 79)',
-      'color: white',
-      'display: block',
-      'border-radius: 3px',
-      'padding: 2px 0'].join(';'),
+    success: [
+      "background: rgb(76, 174, 79)",
+      "color: white",
+      "display: block",
+      "border-radius: 3px",
+      "padding: 2px 0"
+    ].join(";"),
 
-    error: ['background: rgb(235, 59, 36)',
-      'color: white', 'display: block',
-      'border-radius: 3px',
-      'padding: 2px 0'].join(';')
+    error: [
+      "background: rgb(235, 59, 36)",
+      "color: white",
+      "display: block",
+      "border-radius: 3px",
+      "padding: 2px 0"
+    ].join(";")
   }
-})
+});
 
 const state = () => defaultState();
 
 export const mutations = {
   updateField,
   RESET_BASE_STATE(state) {
-    Object.assign(state, defaultState())
+    Object.assign(state, defaultState());
   },
   SET_AUTHENTICATED(state, payload) {
     state.authenticated = payload;
@@ -58,81 +62,83 @@ export const mutations = {
     state.appDataScreenshots = payload;
   },
   PUSH_NEW_APP_DATA_SCREENSHOT(state) {
-    state.appDataScreenshots.push({ new: true, base64Img: '', info: {} })
+    state.appDataScreenshots.push({ new: true, base64Img: "", info: {} });
   },
   PUSH_NEW_APP_DATA_SCREENSHOT_DATA(state, payload) {
     state.appDataScreenshots[payload.index] = {
-      new: true, base64Img: payload.base64, info: payload.info
-    }
+      new: true,
+      base64Img: payload.base64,
+      info: payload.info
+    };
   },
   REMOVE_NEW_APP_DATA_SCREENSHOT(state, payload) {
     state.appDataScreenshots.splice(payload, 1);
   },
   PUSH_NEW_APP_DATA_SECTION(state) {
-    state.appData.sections.push({ new: true, heading: '', body: '' })
+    state.appData.sections.push({ new: true, heading: "", body: "" });
   },
   REMOVE_NEW_APP_DATA_SECTION(state, payload) {
     state.appData.sections.splice(payload, 1);
-  },
-}
+  }
+};
 
 export const actions = {
   resetBaseState({ commit }) {
-    commit('RESET_BASE_STATE')
+    commit("RESET_BASE_STATE");
   },
   setAuthenticated(context, payload) {
-    context.commit('SET_AUTHENTICATED', payload)
+    context.commit("SET_AUTHENTICATED", payload);
   },
   setAppData(context, payload) {
-    context.commit('SET_APP_DATA', payload)
+    context.commit("SET_APP_DATA", payload);
   },
   setAppDataLogo(context, payload) {
-    context.commit('SET_APP_DATA_LOGO', payload)
+    context.commit("SET_APP_DATA_LOGO", payload);
   },
   setAppDataScreenshots(context, payload) {
-    context.commit('SET_APP_DATA_SCREENSHOTS', payload)
+    context.commit("SET_APP_DATA_SCREENSHOTS", payload);
   },
   pushNewAppDataScreenshot(context) {
-    context.commit('PUSH_NEW_APP_DATA_SCREENSHOT')
+    context.commit("PUSH_NEW_APP_DATA_SCREENSHOT");
   },
   pushNewAppDataScreenshotData(context, payload) {
-    context.commit('PUSH_NEW_APP_DATA_SCREENSHOT_DATA', payload)
+    context.commit("PUSH_NEW_APP_DATA_SCREENSHOT_DATA", payload);
   },
   removeNewAppDataScreenshot(context, payload) {
-    context.commit('REMOVE_NEW_APP_DATA_SCREENSHOT', payload)
+    context.commit("REMOVE_NEW_APP_DATA_SCREENSHOT", payload);
   },
   pushNewAppDataSection(context) {
-    context.commit('PUSH_NEW_APP_DATA_SECTION')
+    context.commit("PUSH_NEW_APP_DATA_SECTION");
   },
   removeNewAppDataSection(context, payload) {
-    context.commit('REMOVE_NEW_APP_DATA_SECTION', payload)
+    context.commit("REMOVE_NEW_APP_DATA_SECTION", payload);
   },
   async nuxtServerInit({ commit, state, req }, { app }) {
-
     const cookieRes = this.$cookies.getAll();
 
-    return axios.get(`${process.env.apiHost}/auth/user/`, {
-      withCredentials: true,
-      headers: { Cookie: `sid=${cookieRes.sid}` }
-    })
-      .then((res) => {
-        console.log('nuxtServerInitt', res.data);
+    return axios
+      .get(`${process.env.apiHost}/auth/user/`, {
+        withCredentials: true,
+        headers: { Cookie: `sid=${cookieRes.sid}` }
+      })
+      .then(res => {
+        console.log("nuxtServerInitt", res.data);
 
-        commit('SET_USER', res.data)
-        commit('SET_AUTHENTICATED', res.data.isLoggedIn)
+        commit("SET_USER", res.data);
+        commit("SET_AUTHENTICATED", res.data.isLoggedIn);
 
         return Promise.resolve();
       })
-      .catch((err) => {
-        console.log('nuxtServerInit', err);
+      .catch(err => {
+        console.log("nuxtServerInit", err);
         return Promise.reject();
       });
   }
-}
+};
 
 export const getters = {
   getField
-}
+};
 
 export default {
   namespaced: true,
@@ -140,5 +146,5 @@ export default {
   mutations,
   actions,
   getters,
-  state,
+  state
 };
